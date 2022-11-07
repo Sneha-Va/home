4c5d5a9bfc8e1702dffe0a9c72730502914f4e49
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-07 09:57:29 +0530

    connevted

--- home.py
+++ home.py
@@ -1,12 +1,24 @@
+import mysql.connector
+mydb=mysql.connector.connect(host='localhost',user='root',password='',database='homeautomationdb')
+mycursor = mydb.cursor()
 while True:
     print("select an option from the menu")
     print('1 insert')
     print('2 view')
     print('3 search')
     print('4 exit')
-    choice=int(input('enter the option:'))
+    choice=int(input("enter the option:"))
     if(choice==1):
         print("insert details")
+        temprature=input('enter tempreature:')
+        hummidity=input("enter hummidity:")
+        moisture=input('enter moisture:')
+        date=input("enter date:")
+        sql='INSERT INTO `sensorvalue`(` `temprature`, `hummidity`, `moisture`, `date`) VALUES (%s,%s,%s,%s)'
+        data=(temprature,hummidity,moisture,date)
+        mycursor.execute(sql,data)
+        mydb.commit()
+        print("view employee")
     if(choice==2):
         print("view details")
     if(choice==3):
