eff346d5d0b35abfd7b1b0323939eef6921ce408
Sneha-Va <sneha.varghese@nestgroup.net>
2022-11-07 09:32:58 +0530

    first commit

---
+++ home.py
@@ -0,0 +1,20 @@
+while True:
+    print("select an option from the menu")
+    print('1 insert')
+    print('2 view')
+    print('3 search')
+    print('4 exit')
+    choice=int(input('enter the option:'))
+    if(choice==1):
+        print("insert details")
+    if(choice==2):
+        print("view details")
+    if(choice==3):
+        print(" search details")
+    if(choice==4):
+        print("exit")
+        break;
+    
+        
+            
+        
